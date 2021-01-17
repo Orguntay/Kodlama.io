6bc978783c17749bce5589326abaed5048bb6564
Atakan Cevat COŞKUNDERE <a.cevat77@gmail.com>
2021-01-18 01:01:48 +0300

    Day 3

---
+++ ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        public void AddCustomer(Customer customer)
+        {
+            customers.Add(customer);
+        }
+        public void GetCustomerList()
+        {
+            foreach (var item in customers)
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Müşteri Adı :" + item.Name);
+                Console.WriteLine("Müşteri Soyadı :" + item.Surname);
+            }
+        }
+        public void DeleteCustomer(Customer customer)
+        {
+            customers.Remove(customer);
+            Console.WriteLine(customer.Name + " " + customer.Surname + " Has been deleted successfully");
+        }
+        public void UpdateCustomer(Customer customer)
+        {
+            customer.Surname = customer.Surname + " UPDATED";
+            customers.FirstOrDefault(x => x.Id == customer.Id).Surname = customer.Surname;
+            Console.WriteLine(customer.Name + " " + customer.Surname + " Has been updated successfully");
+        }
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Oluiturulan müşteriler
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Atakan Cevat";
+            customer1.Surname = "COŞKUNDERE";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Aleyna";
+            customer2.Surname = "Tilki";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Ben";
+            customer3.Surname = "FERO";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.Name = "Killa";
+            customer4.Surname = "HAKAN";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            // Müşterilerin eklendiği methodlar
+            customerManager.AddCustomer(customer1);
+            customerManager.AddCustomer(customer2);
+            customerManager.AddCustomer(customer3);
+            customerManager.AddCustomer(customer4);
+
+
+            // Müşterilerin silindiği metodlar
+            //customerManager.DeleteCustomer(customer2);
+
+            ////Müşterilerin Güncellendiği metodlar
+            customerManager.UpdateCustomer(customer3);
+
+            //Tüm müşterilerin gösterilmesi
+            customerManager.GetCustomerList();
+
+        }
+    }
+}
